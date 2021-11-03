ae9bf485e581facf3143b0fad25b50d1ec8430bc
Jonathan Chagolla <90534449+chagolla23@users.noreply.github.com>
2021-11-02 22:50:41 -0700

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,59 @@
+def calculate():
+    operation = input('''
+Please type in the math operation you would like to complete:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
+
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+    # Add again() function to calculate() function
+    again()
+
+def again():
+    calc_again = input('''
+Do you want to calculate again?
+Please type Y for YES or N for NO.
+''')
+
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('Thanks for coming!')
+    else:
+        again()
+
+calculate()
+
+
+def create_pyramid():
+    rows = int(input("Enter the number of rows: "))
+    for row in range(rows):
+        for _ in range(rows-row-1):
+            print(end=" ")
+        for _ in range(row+1):
+            print("X", end=" ")
+        print(" ")
+create_pyramid()
